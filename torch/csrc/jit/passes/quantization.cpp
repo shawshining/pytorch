6517d963c4197f01c817005e2a4ded0cceb45056
Jerry Zhang <jerryzh168@gmail.com>
2020-03-28 10:29:55 -0700

    [quant][graphmode] Quantization support for at::sort
    Summary:
    
    Test Plan:
    python test/test_jit.py
    
    Reviewers:
    .
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 37a24a2c8ac01158992f8667b35a8b5abb2fae44
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/35571

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -193,9 +193,6 @@ std::vector<Value*> getPassThroughInputs(Value* v) {
       "permute",
       "repeat_interleave",
       "relu",
-      // TODO: sort returns a tuple of Tensors, we have
-      // to extend the API to support that
-      // "sort",
   };
   if (isFunctionNode(
           n,
@@ -209,7 +206,8 @@ std::vector<Value*> getPassThroughInputs(Value* v) {
                  // We don't have call functions
                  // after inline
                  /* call_funcs = */ {},
-                 /* aten_funcs = */ single_input_aten_funcs)) {
+                 /* aten_funcs = */ single_input_aten_funcs) ||
+             n->kind() == Symbol::aten("sort") && v->offset() == 0) {
     return {n->input(0)};
   } else if (n->kind() == prim::If &&
              n->outputs().size() == 1) {
